645e1358fd22be7edef3f921a174e75f1f80f1bb
z.shajarkar <z.shajarkar@fanap.ir>
2023-05-02 17:44:03 +0330

    Resolved config/8:Configure swagger

--- src/main/java/com/example/NetWorthApplication.java
+++ src/main/java/com/example/NetWorthApplication.java
@@ -2,9 +2,6 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -18,11 +15,12 @@ import java.util.Properties;
 @Configuration
 @EnableJpaRepositories(basePackages = "com.example.repository")
 @SpringBootApplication
-public class NetWorthApplication  {
+public class NetWorthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NetWorthApplication.class, args);
     }
+
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
@@ -42,6 +40,7 @@ public class NetWorthApplication  {
         dataSource.setPassword("");
         return dataSource;
     }
+
 
     private Properties jpaProperties() {
         Properties properties = new Properties();
--- src/main/java/com/example/config/WebSecurityConfig.java
+++ src/main/java/com/example/config/WebSecurityConfig.java
@@ -50,7 +50,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
-                .authorizeRequests().antMatchers("/users/**").permitAll().
+                .authorizeRequests().antMatchers("/users/**").permitAll()
+                .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll().
                 anyRequest().authenticated().and().
                 exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
